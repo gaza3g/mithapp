075de23dd14d06b8ded486083cb1776e79aa8f46
Ghazaly <gazaly@gmail.com>
2015-05-21 11:44:54 +0800

    Initial commit.

---
+++ src/components/contacts/contacts.js
@@ -0,0 +1,15 @@
+
+Contacts = {}
+
+Contacts.model = function () {
+  // Model code goes here
+};
+
+Contacts.controller = function () {
+  var ctrl = this;
+  // Controller code goes here
+}
+
+Contacts.view = function (ctrl) {
+  // View code goes here
+}
