a4d529ace5474afd5d84b1fa4c159e8a5e32aa8d
Ghazaly <gazaly@gmail.com>
2015-05-21 12:01:20 +0800

    Added User controller.

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -8,3 +8,21 @@ Users.model = function () {
 	this.lms_domain = m.prop('[Your lms domain]');
 	this.db_instance = m.prop('[Your db instance]');
 }
+
+
+Users.controller = function() {
+	var ctrl = this
+
+	// Keep track of all entered attendees. We can
+	// do this with an array:
+
+	// Initialize with empty model so we'll see one empty
+	// fieldset in the page when we implement the view.
+
+	// We used m.prop here to follow the Uniform access 
+	// principal as well as retaining the flexibility to 
+	// later turn this into a computed property using
+	// m.request.
+	ctrl.users = m.prop([new Users.model()])
+}
+
